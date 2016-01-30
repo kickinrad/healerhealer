79b9badc197c261ba78f62e942a790cda4aa20fa
Nigel Nixon <nigel.nixon@gmail.com>
2016-01-29 21:10:31 -0800

    changed version

--- conf.lua
+++ conf.lua
@@ -1,13 +1,13 @@
 --[[ To commit from console/terminal:
 	git add *
+	git commit -m [I, include description of changes, Esc, shift + :, wq, enter]
 	git tag -a [Version #]
-	git commit [I, include description of changes, Esc, shift + :, wq, enter]
 	git push origin
 --]]
 
 function love.conf(t)
 	t.title = "Global Game Jam 2016" -- The title of the window the game is in (string)
-	t.version = "0.9.1"         -- The LÖVE version this game was made for (string)
+	t.version = "0.10.0"         -- The LÖVE version this game was made for (string)
 	t.window.width = 800
 	t.window.height = 600
 
