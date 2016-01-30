e213a96ca1f39eb4fb41bf96e65cb637bcd84a63
Joshua Jacobs <Josh@calvisitor-10-105-136-97.calvisitor.1918.berkeley.edu>
2016-01-29 20:47:53 -0800

    Added main.lua and conf.lua.

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.title = "Global Game Jam 2016" -- The title of the window the game is in (string)
+	t.version = "0.9.1"         -- The LÖVE version this game was made for (string)
+	t.window.width = 800
+	t.window.height = 600
+
+	-- For Windows debugging
+	t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,13 @@
+-- Global Game Jam 2016
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+  love.graphics.print("Hello World", 350, 300)
+end
