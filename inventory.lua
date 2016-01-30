0a2ab9ac95fd98cc883ae86d51f7b6219edd034e
Joshua Jacobs <Josh@calvisitor-10-105-136-97.calvisitor.1918.berkeley.edu>
2016-01-29 21:55:16 -0800

    Added inv.lua and some debug features to main.lua

---
+++ inventory.lua
@@ -0,0 +1,11 @@
+-- inventory.lua
+
+inventory = {
+  invBox = {
+    x = 25,
+    y = 25,
+    w = 300,
+    h = 300
+  },
+  buttons = {}
+}
--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 -- Global Game Jam 2016
 
+debug = true
+
+require "inventory"
+
 function love.load()
 
 end
@@ -9,5 +13,17 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.print("Hello World", 350, 300)
+  love.graphics.line(350, 0, 350, 600) -- split patient/inventory sections
+  love.graphics.rectangle("line", 25, 25, 300, 300) -- inventory box -- extend to left and top edges
+  love.graphics.rectangle("line", 350, 0, 450, 100) -- dialogue box
+  love.graphics.rectangle("line", 350, 400, 450, 200) -- reference manual box
+  if debug then
+    love.graphics.print(tostring(love.timer.getFPS()), 5, 5) -- fps
+    love.graphics.setColor(255, 0, 0, 255)
+    love.graphics.print("X     Y", 5, 560)
+    love.graphics.print(tostring(love.mouse.getX()), 5, 575) -- mouse x coords
+    love.graphics.print(",", 29, 575)
+    love.graphics.print(tostring(love.mouse.getY()), 32, 575) -- mouse x coords
+    love.graphics.setColor(255, 255, 255, 255)
+  end
 end
